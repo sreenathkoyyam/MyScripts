3223276e2340cb74193d2e4618cb00a6c185e758
Sreenath Koyyam <sreenath886@gmail.com>
2016-11-23 15:40:41 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,30 @@
+1) who am i
+Calendar
+-----------
+ cal :: for calendar
+ cal 7 2006
+  cal feb 2033
+Date
+------
+3) date :: current date
+4) date ‘+DATE:%m-%y%nTime:%H:%M:%S’  :: custom date display
+Touch
+---------
+touch test1  :: create a file
+Directory
+------------
+mkdir  <Folder-Name>:: make a directory
+
+CAT
+------
+Cat > random  [press ctrl + d]
+hii
+Cat random  :: show random file content
+Cat random test >sample   :: Put random and test file content in sample (Merge 2 File)
+
+Clear
+--------
+Clear   :: will clear the terminal
+
+Rename and Delete
+---------------------------
